a83295f448489b674220ed89a8282a09dde6edf6
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2018-02-13 17:23:45 -0500

    added background gradient

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 class Home extends Component {
     state = {
@@ -31,18 +32,46 @@ class Home extends Component {
             return <div>{this.state.error}</div>
         }
         return (
-            <div>
-                <h1>Jeans Jams: Travel Edition</h1>
-                <h2>All Cities</h2>
-                {this.state.cities.map(city => (
-                    <div key={city.id}>
-                        <Link to={`/cities/${city.id}`}>{city.name}</Link>
-                        <div><img width="200" src={city.photo_url} alt="picture of city" /></div>
-                    </div>
-                ))}
-            </div>
+            <Background>
+                <div>
+                    <h1>Jeans Jams: Travel Edition</h1>
+                    <h2>All Cities</h2>
+                    {this.state.cities.map(city => (
+                        <div key={city.id}>
+                            <Link to={`/cities/${city.id}`}>{city.name}</Link>
+                            <div><img width="200" src={city.photo_url} alt="picture of city" /></div>
+                        </div>
+                    ))}
+                </div>
+            </Background>
         )
     }
 }
 
 export default Home
+
+const Background = styled.div`
+  width: 100%;
+  position: absolute;
+  background: linear-gradient(270deg, #003366, #b27000, #06617d, #067370);
+  background-size: 800% 800%;
+  -webkit-animation: colors 30s ease infinite;
+  -moz-animation: colors 30s ease infinite;
+  animation: colors 30s ease infinite;
+
+@-webkit-keyframes colors {
+  0%{background-position:0% 50%}
+  50%{background-position:100% 50%}
+  100%{background-position:0% 50%}
+}
+@-moz-keyframes colors {
+  0%{background-position:0% 50%}
+  50%{background-position:100% 50%}
+  100%{background-position:0% 50%}
+}
+@keyframes colors { 
+  0%{background-position:0% 50%}
+  50%{background-position:100% 50%}
+  100%{background-position:0% 50%}
+}
+`
