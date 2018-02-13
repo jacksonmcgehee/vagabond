b0b33f72522190c1d3edb7da50463811613430b8
jacksonmcgehee <jacksonmcgehee@gmail.com>
2018-02-13 12:26:45 -0500

    Made index and destroy routes for the comments_controller.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,9 +1,17 @@
 class Api::CommentsController < ApplicationController
   def index
+    @city = City.find(params[:city_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:post_id]).comments
+
+    @response = {city: @city, post: @post, comment: @comment}
     
+    render json: @response
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id]).destroy
+    render status: :ok
   end
+
 end
