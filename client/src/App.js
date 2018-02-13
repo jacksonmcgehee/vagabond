6921c32a12e3dce08b0e4d991286080d611cafe3
DenimCity <altidorj1@gmail.com>
2018-02-13 11:46:46 -0500

    refactored app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from "react-router-dom"
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import Home from './components/Home'
 import CityPage from './components/CityPage'
